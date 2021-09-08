7aa178b0762311b1323be02ac55499559e8cd05f
Jacques de Laval <Jacques.De.Laval@westermo.com>
2021-09-08 16:17:22 +0200

    Add test case for IPv4 addresses mapped on IPv6
    Signed-off-by: Jacques de Laval <Jacques.De.Laval@westermo.com>

---
+++ test/accept6-ipv4-mapped.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#set -x
+
+# shellcheck source=/dev/null
+. "$(dirname "$0")/lib.sh"
+
+topology
+server -6 -t
+
+print "Verifying loopback connectivity ..."
+./client -t -p 8080 127.0.0.1 || FAIL
+
+print "Verifying no connection via a1 ..."
+./client -t -p 8080 10.0.0.1 && FAIL
+
+print "Verifying connection via a2 ..."
+./client -t -p 8080 20.0.0.1 || FAIL
+
+OK
